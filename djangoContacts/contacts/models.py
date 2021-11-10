0894a94050babe5c422ec551ee76075e4dc06066
juliagsy <67888047+juliagsy@users.noreply.github.com>
2021-11-10 15:42:06 +0800

    fix django models, forms, serializers

--- djangoContacts/contacts/models.py
+++ djangoContacts/contacts/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 # Create your models here.
 class Contact(models.Model):
-    name = models.TextField()
-    number = models.TextField()
+    name = models.CharField(max_length=200)
+    number = models.CharField(max_length=100)
--- djangoContacts/contacts/views.py
+++ djangoContacts/contacts/views.py
@@ -1,17 +1,38 @@
 from django.http import HttpResponse
 from django.core import serializers
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
 
 from .models import Contact
+from .serializers import ContactSerializer
+from .form import ContactForm
 
 # Create your views here.
+@api_view(['GET', 'POST'])
 def retrieve(request):
+    context = {}
+
     if request.method == 'GET':
         allcontacts = Contact.objects.all()
         output = serializers.serialize('json', allcontacts)
         return HttpResponse(output, 'application/json')
 
-    # elif request.method == 'POST':
+    elif request.method == 'POST':
+        serializer = ContactSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return HttpResponse(status=204)
 
+        # form = ContactForm(request.POST)
+        # context['form'] = form
+        # if form.is_valid():
+        #     form.save()
+        #     return HttpResponse(status=204)
+        # return render(request, 'index.html', context)
+
+@api_view(['GET', 'PUT', 'DELETE'])
 def show(request, contact_id):
     contact = Contact.objects.get(id = contact_id)
 
@@ -22,3 +43,9 @@ def show(request, contact_id):
     elif request.method == 'DELETE':
         contact.delete()
         return HttpResponse(status=204)
+
+    # elif request.method == 'PUT':
+    #     serializer = StudentSerializer(student, data=request.data,context={'request': request})
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return Response(status=status.HTTP_204_NO_CONTENT)
